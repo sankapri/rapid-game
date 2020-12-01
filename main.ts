e0a76c4b39b1f23d1fca1ea96e5411c0dae8a271
sankapri <75212267+sankapri@users.noreply.github.com>
2020-12-01 10:59:16 +0530

    Handled registering input from only first player. Keep track of score & show winner on pressing B

--- main.ts
+++ main.ts
@@ -1,32 +1,53 @@
 input.onPinPressed(TouchPin.P0, function () {
-    if (Is_A_pressed == 1) {
+    if (Game_active == 1) {
+        Game_active = 0
+        Score0 += 1
         basic.showString("S")
-        basic.pause(2000)
-        control.reset()
+        basic.pause(3000)
+        basic.showString("A")
     }
 })
 input.onButtonPressed(Button.A, function () {
-    Is_A_pressed = 1
+    Game_active = 1
     basic.showString("G")
 })
 input.onPinPressed(TouchPin.P2, function () {
-    if (Is_A_pressed == 1) {
+    if (Game_active == 1) {
+        Game_active = 0
+        Score2 += 1
         basic.showString("R")
-        basic.pause(2000)
-        control.reset()
+        basic.pause(3000)
+        basic.showString("A")
     }
 })
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+input.onButtonPressed(Button.B, function () {
+    if (Score0 > Score1 && Score0 > Score2) {
+        basic.showString("S")
+        basic.pause(3000)
+        control.reset()
+    } else if (Score1 > Score2) {
+        basic.showString("P")
+        basic.pause(3000)
+        control.reset()
+    } else {
+        basic.showString("R")
+        basic.pause(3000)
+        control.reset()
+    }
 })
 input.onPinPressed(TouchPin.P1, function () {
-    if (Is_A_pressed == 1) {
+    if (Game_active == 1) {
+        Game_active = 0
+        Score1 += 1
         basic.showString("P")
-        basic.pause(2000)
-        control.reset()
+        basic.pause(3000)
+        basic.showString("A")
     }
 })
-let Is_A_pressed = 0
+let Score1 = 0
+let Score2 = 0
+let Score0 = 0
+let Game_active = 0
 basic.showString("A")
 basic.forever(function () {
 	
